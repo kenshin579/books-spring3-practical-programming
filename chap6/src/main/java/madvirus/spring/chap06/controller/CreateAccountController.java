4f5740af720eda166e2853a8b08b9f033b902ebc
Administrator <user@example.com>
2016-06-01 22:10:00 +0900

    chap6 스터디중

--- chap6/src/main/java/madvirus/spring/chap06/controller/CreateAccountController.java
+++ chap6/src/main/java/madvirus/spring/chap06/controller/CreateAccountController.java
@@ -11,11 +11,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 
+/*
+    - GET 요청시 폼의 특정값을 초기화 함
+    - POST때는 form에 입력한 값을 받음 사용자로부터
+ */
 @Controller
 @RequestMapping("/account/create.do")
 public class CreateAccountController {
 
-    @ModelAttribute("command")
+    /*
+
+    todo: formBacking은 언제 누구의 의해서 호출되나?
+    -
+
+    - @ModelAttribute 어노테이션이 적용된 메서드가 @RequestMapping 어노테이션이 적용된 메서드 보다 먼저 호출된다
+
+    */
+    @ModelAttribute("command") //submit 메서드의 파라미터에서 사용된 @ModelAttribute 어노테이션의 값이 command로 동일함
     public MemberInfo formBacking(HttpServletRequest request) {
         if (request.getMethod().equalsIgnoreCase("GET")) {
             MemberInfo mi = new MemberInfo();
@@ -24,7 +36,7 @@ public class CreateAccountController {
             mi.setAddress(address);
             return mi;
         } else {
-            return new MemberInfo();
+            return new MemberInfo(); //command 라는 이름의 모델 객체로 저장됨
         }
     }
 
@@ -37,6 +49,10 @@ public class CreateAccountController {
         return "account/creationForm";
     }
 
+    /*
+    forBacking() 메서드의 반환값 MemberInfo()는 command 이름의 모델 객체로 저장되고
+    command MemberInfo 객체는 submit() 메서드의 동일한 모델 이름을 갖는 커맨드 객체(첫번째 파라미터)로 전달됨
+     */
     @RequestMapping(method = RequestMethod.POST)
     public String submit(@ModelAttribute("command") MemberInfo memberInfo,
                          BindingResult result) {
